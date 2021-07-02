6b8bce7d6c2267dd55322809867bbe6c6efdbca4
Marwah Bakoor <marwahbakoor@gmail.com>
2021-07-02 04:54:54 +0300

    Pass data from components that have it to components that need it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Search from './Search';
 import ListBooks from './ListBooks';
-// import * as BooksAPI from './BooksAPI'
+
 import './App.css';
 
 class BooksApp extends React.Component {
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -3,7 +3,50 @@ import CurrentlyReading from './CurrentReading';
 import WantToRead from './WantToRead';
 import Read from './Read';
 import OpenSearch from './OpenSearch';
+import * as BooksAPI from './BooksAPI'
 class ListBooks extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { 
+      currentlyReading : [],
+      wantToRead : [],
+      read: []
+    }
+  }
+
+  filterMyBooks = (books) =>{
+    books.filter((book)=>{
+      switch(book.shelf) {
+        case "currentlyReading":
+          this.setState((books) => ({
+           currentlyReading: [...books.currentlyReading, book ]
+          }))
+          break;
+        case "wantToRead":
+          this.setState((books) => ({
+            wantToRead: [...books.wantToRead, book ]
+          }))
+          break;
+        default:
+
+          this.setState((books) => ({
+            read: [...books.read, book ]
+          }))
+      }
+
+    })
+
+  }
+  
+
+  componentDidMount() {
+    BooksAPI.getAll()
+    .then((books)=>{
+      this.filterMyBooks(books)
+    })
+    
+  }
 
     render(){
         return(
@@ -13,9 +56,9 @@ class ListBooks extends Component {
             </div>
             <div className="list-books-content">
               <div>
-                <CurrentlyReading />
-                <WantToRead />
-                <Read /> 
+                <CurrentlyReading books={this.state.currentlyReading} />
+                <WantToRead books={this.state.wantToRead} />
+                <Read books={this.state.read} /> 
               </div>
 
               <OpenSearch />
